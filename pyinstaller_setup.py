518fcb2e0f06774b54ec5c14175e9888c443bac9
ChsHub <christian1193@web.de>
2018-04-02 18:46:30 +0200

    - pyinstaller spec

--- pyinstaller_setup.py
+++ pyinstaller_setup.py
@@ -1,4 +1,4 @@
-# pyinstaller __main__.py --noconsole
+# pyinstaller __main__.py --noconsole --onedir
 # pyinstaller __main__.spec --noconsole
 # EXE(
 # Tree('.\\lib\\ffmpeg'),
@@ -9,7 +9,8 @@ from utility.os_interface import delete_file
 delete_file('build')
 delete_file('dist')
 process = Popen('pyinstaller __main__.spec')
+# process = Popen('pyinstaller __main__.py --noconsole --onedir')
 
 print(process.communicate(input="y"))
-process = Popen('./dist/__main__/__main__.exe > ./log_files/exe.log')
-print(process.communicate(input="y"))
+process = Popen('./dist/__main__/__main__.exe >log_files/execute.log')
+print(process.communicate())
