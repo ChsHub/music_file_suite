0cb714676815da56123042c406c6391d661e5169
ChsHub <christian1193@web.de>
2018-11-07 22:40:51 +0100

    downloader: remove unused lines

--- src/model/apps/downloader.py
+++ src/model/apps/downloader.py
@@ -1,12 +1,10 @@
-# -*- coding: utf8 -*-
-from logging import info, exception
+from logging import info
 from re import findall
 from subprocess import PIPE, Popen, DEVNULL
 from threading import BoundedSemaphore
 
 from utility.encoding import decode
-from utility.os_interface import get_cwd, change_dir, get_file_count, rename_file, remove_file_ending
-from utility.utilities import get_file_type
+from utility.os_interface import get_cwd, change_dir
 
 from src.resource.paths import downloader_command, path_to_download_dir
 
@@ -27,18 +25,9 @@ class Downloader:
         with self._Download_sem:
             info("DOWNLOAD: " + url)
             os_dir = get_cwd()
-            file_count = get_file_count(path_to_download_dir)
 
             change_dir(path_to_download_dir)
             process = Popen(downloader_command + [url], stdin=DEVNULL, stdout=PIPE, stderr=PIPE, shell=True)
-
-
-            file_name = ''
-            for line in process.stdout:
-                line = decode(line)
-                if "Destination: " in line:
-                    file_name = line.split("Destination: ")[-1].strip()
-                    break
 
             for line in process.stdout:
                 line = decode(line)
