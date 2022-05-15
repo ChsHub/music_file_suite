a67b9e860a207f9a3e70b420a223faac8bc2b298
ChsHub <christian1193@web.de>
2022-05-15 11:16:35 +0200

    setup_nsi.py: use pathlib;

--- setup_nsi.py
+++ setup_nsi.py
@@ -1,18 +1,17 @@
+from os import walk
 from os.path import join
-
-from utility.os_interface import read_file_data, write_file_data, depth_search_files
-
+from pathlib import Path
 from src.resource.ConfigReader import ConfigReader
 
 icon_path = ConfigReader()['Window']['icon_path']
 text_view_title = ConfigReader()['Window']['text_view_title']
 
 script_name = "installer.nsi"
-script = read_file_data("installer_template.nsi")
+script = Path("installer_template.nsi").read_text('UTF-8')
 
 script = script.replace("App Name", text_view_title)
 script = script.replace("logo.ico", icon_path)
 
-files = depth_search_files(join('./dist', text_view_title), "")
+files = [file for root, _, files in walk(Path('./dist', text_view_title)) for file in files]
 
-write_file_data(".", script_name, script)
+Path(script_name).write_text(script)
